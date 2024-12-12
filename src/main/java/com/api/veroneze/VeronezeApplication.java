525092053651379f47429e676170491e4e2e4c63
Lucas Carlos <devlucas17@gmail.com>
2024-12-12 00:36:17 -0300

    feat: first commit

---
+++ src/main/java/com/api/veroneze/VeronezeApplication.java
@@ -0,0 +1,13 @@
+package com.api.veroneze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeronezeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeronezeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/veroneze/VeronezeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.veroneze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeronezeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
