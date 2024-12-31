65a6826d983ea89b37ca3b03abb2d9cbdb7c685e
Lucas Carlos <devlucas17@gmail.com>
2024-12-30 23:46:22 -0300

    feat: implementando CorsMappings

---
+++ src/main/java/com/api/veroneze/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.api.veroneze.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*") //Permite todos os endpoints
+                .allowedOrigins("http://127.0.0.1:5500") //origem permitida
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*"); //Cabeçalho permitidos
+    }
+}
